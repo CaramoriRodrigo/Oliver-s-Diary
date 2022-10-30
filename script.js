e4638cca6f06b3416342ee164cdca497ee21ddd7
CaramoriRodrigo <108237381+CaramoriRodrigo@users.noreply.github.com>
2022-10-30 19:51:48 -0300

    modificação para fins de teste

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const wordsQuantityElement = document.getElementById("wordsQuantity");
 const powerUp = document.getElementById("powerUpCard")
 let wordsQuantity = 0;
 let clickPower = 1;
+let PowerUpClickLevel = 0;
 
 
 
@@ -15,10 +16,19 @@ function powerUpBuy(){
     clickPower=2;
     powerUp.remove();
     wordsQuantity = wordsQuantity - 100;
+    PowerUpClickLevel++
     alert("You increase in one your writing power!");
     }else{
         alert("You don't have 100 words yet...")
     }
 }
 book.addEventListener("click",clickEvent)
-powerUp.addEventListener("click",powerUpBuy)
+powerUpBuyButton.addEventListener("click",powerUpBuy)
+
+
+// Localização do Mouse
+
+document.querySelector('body').addEventListener('mousemove', function(event) {
+    var posX = event.clientX,
+        posY = event.clientY;
+  });
