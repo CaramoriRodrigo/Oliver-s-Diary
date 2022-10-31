93280ee4747770adcffc5ab4e37e5980f9ccd0d8
CaramoriRodrigo <108237381+CaramoriRodrigo@users.noreply.github.com>
2022-10-30 23:58:35 -0300

    A.0.03

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const book = document.getElementById("book");
+const wpsValue = document.getElementById("wpsValue");
 const wordsQuantityElement = document.getElementById("wordsQuantity");
 const powerUp = document.getElementById("powerUpImg");
 const powerUp2 = document.getElementById("powerUpImg2");
+const powerUp3 = document.getElementById("powerUpImg3");
 const powerUpDesc = document.getElementById("powerUpDesc");
 const powerUpDesc2 = document.getElementById("powerUpDesc2");
+const powerUpDesc3 = document.getElementById("powerUpDesc3");
 let wordsQuantity = 0;
 let clickPower = 1;
+let wpsPower = 0;
 let PowerUpClickLevel = 0;
 
 
@@ -19,6 +23,7 @@ function powerUpBuy1(){
     clickPower=2;
     document.getElementById("powerUp1").remove();
     wordsQuantity = wordsQuantity - 100;
+    wordsQuantityElement.textContent = wordsQuantity;
     PowerUpClickLevel++
     alert("You increase in one your writing power!");
     }else{
@@ -29,16 +34,30 @@ function powerUpBuy2(){
     if(wordsQuantity >= 250){
     clickPower=3;
     document.getElementById("powerUp2").remove();
-    wordsQuantity = wordsQuantity - 100;
+    wordsQuantity = wordsQuantity - 250;
+    wordsQuantityElement.textContent = wordsQuantity;
     PowerUpClickLevel++
     alert("You increase in one your writing power!");
     }else{
         alert("You don't have 250 words yet...")
     }
 }
+function powerUpBuy3(){
+    if(wordsQuantity >= 500){
+    document.getElementById("powerUp3").remove();
+    wordsQuantity = wordsQuantity - 500;
+    wordsQuantityElement.textContent = wordsQuantity;
+    wpsPower++
+    wpsStart();
+    alert("You increase in one your writing power por second!");
+    }else{
+        alert("You don't have 500 words yet...")
+    }
+}
 book.addEventListener("click",clickEvent)
 powerUp.addEventListener("click",powerUpBuy1)
 powerUp2.addEventListener("click",powerUpBuy2)
+powerUp3.addEventListener("click",powerUpBuy3)
 
 window.addEventListener("mousemove",function(e) {   
     var X = e.clientX,
@@ -47,6 +66,8 @@ window.addEventListener("mousemove",function(e) {
         powerUpDesc.style.left = (X + 20)  + 'px';
         powerUpDesc2.style.top = (Y + 20)  + 'px';
         powerUpDesc2.style.left = (X + 20)  + 'px';
+        powerUpDesc3.style.top = (Y + 20)  + 'px';
+        powerUpDesc3.style.left = (X + 20)  + 'px';
         PowerUpToolTip();
   });
 
@@ -55,4 +76,13 @@ function PowerUpToolTip(){
     powerUp.addEventListener("mouseleave",function(){powerUpDesc.style.visibility = "hidden";})
     powerUp2.addEventListener("mouseover",function(){powerUpDesc2.style.visibility = "visible";})
     powerUp2.addEventListener("mouseleave",function(){powerUpDesc2.style.visibility = "hidden";})
+    powerUp3.addEventListener("mouseover",function(){powerUpDesc3.style.visibility = "visible";})
+    powerUp3.addEventListener("mouseleave",function(){powerUpDesc3.style.visibility = "hidden";})
+}
+function wpsStart(){
+    setInterval(() => {
+        wordsQuantity = wordsQuantity + wpsPower
+        wordsQuantityElement.textContent = wordsQuantity;
+        wpsValue.textContent = wpsPower;
+    }, 1000);
 }
