9110456c7538cc6b23e41d8550089521547138de
CaramoriRodrigo <108237381+CaramoriRodrigo@users.noreply.github.com>
2022-10-30 23:34:03 -0300

    A.0.02

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const book = document.getElementById("book");
 const wordsQuantityElement = document.getElementById("wordsQuantity");
-const powerUp = document.getElementById("powerUpImg")
-const powerUpDesc = document.getElementById("powerUpDesc")
+const powerUp = document.getElementById("powerUpImg");
+const powerUp2 = document.getElementById("powerUpImg2");
+const powerUpDesc = document.getElementById("powerUpDesc");
+const powerUpDesc2 = document.getElementById("powerUpDesc2");
 let wordsQuantity = 0;
 let clickPower = 1;
 let PowerUpClickLevel = 0;
@@ -12,10 +14,10 @@ function clickEvent(){
     wordsQuantity = wordsQuantity + clickPower
     wordsQuantityElement.textContent = wordsQuantity;
 }
-function powerUpBuy(){
+function powerUpBuy1(){
     if(wordsQuantity >= 100){
     clickPower=2;
-    powerUp.remove();
+    document.getElementById("powerUp1").remove();
     wordsQuantity = wordsQuantity - 100;
     PowerUpClickLevel++
     alert("You increase in one your writing power!");
@@ -23,18 +25,34 @@ function powerUpBuy(){
         alert("You don't have 100 words yet...")
     }
 }
+function powerUpBuy2(){
+    if(wordsQuantity >= 250){
+    clickPower=3;
+    document.getElementById("powerUp2").remove();
+    wordsQuantity = wordsQuantity - 100;
+    PowerUpClickLevel++
+    alert("You increase in one your writing power!");
+    }else{
+        alert("You don't have 250 words yet...")
+    }
+}
 book.addEventListener("click",clickEvent)
-powerUp.addEventListener("click",powerUpBuy)
+powerUp.addEventListener("click",powerUpBuy1)
+powerUp2.addEventListener("click",powerUpBuy2)
 
 window.addEventListener("mousemove",function(e) {   
     var X = e.clientX,
         Y = e.clientY;
         powerUpDesc.style.top = (Y + 20)  + 'px';
         powerUpDesc.style.left = (X + 20)  + 'px';
+        powerUpDesc2.style.top = (Y + 20)  + 'px';
+        powerUpDesc2.style.left = (X + 20)  + 'px';
         PowerUpToolTip();
   });
 
 function PowerUpToolTip(){
     powerUp.addEventListener("mouseover",function(){powerUpDesc.style.visibility = "visible";})
     powerUp.addEventListener("mouseleave",function(){powerUpDesc.style.visibility = "hidden";})
+    powerUp2.addEventListener("mouseover",function(){powerUpDesc2.style.visibility = "visible";})
+    powerUp2.addEventListener("mouseleave",function(){powerUpDesc2.style.visibility = "hidden";})
 }
